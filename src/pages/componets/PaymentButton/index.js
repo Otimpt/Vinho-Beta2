4204e44e88e3123f8822b7d993d1e069f0f33d0f
Otimpt <86795444+Otimpt@users.noreply.github.com>
2024-06-11 19:39:31 -0300

    Update index.js

--- src/pages/componets/PaymentButton/index.js
+++ src/pages/componets/PaymentButton/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import SQLite from 'react-native-sqlite-storage';
+
+const db = SQLite.openDatabase({ name: 'ComprasFeitas.db', createFromLocation: '../assets/ComprasFeitas.db' });
 export default function PaymentButton({ onClick }){
     return(
         <View style={styles.container}>

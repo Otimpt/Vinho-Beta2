c8273dea43c574514a175dd80406dc85403fa0cd
Otimpt <86795444+Otimpt@users.noreply.github.com>
2024-06-11 00:55:46 -0300

    Update index.js

--- src/pages/componets/BuyButton/index.js
+++ src/pages/componets/BuyButton/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import SQLite from 'react-native-sqlite-storage';
+
+const db = SQLite.openDatabase({ name: 'ComprasFeitas.db', createFromLocation: '~ComprasFeitas.db' });
 export default function Button({ onClick }){
     return(
         <View style={styles.container}>

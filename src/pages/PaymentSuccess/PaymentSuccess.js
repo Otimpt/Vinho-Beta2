861aa7a19e1d1d6bd493020137168c568c13853b
Otimpt <86795444+Otimpt@users.noreply.github.com>
2024-06-11 00:05:13 -0300

    Update PaymentSuccess.js

--- src/pages/PaymentSuccess/PaymentSuccess.js
+++ src/pages/PaymentSuccess/PaymentSuccess.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-
+import SQLite from 'react-native-sqlite-storage';
 export default function PaymentSuccess({ route, navigation }){
     const { item } = route.params || {};
 
